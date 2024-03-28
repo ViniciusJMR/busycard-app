e71100dc0f0b3b298846793fc6b80f79c9a29093
ViniciusJMR <viniciusdejesus605@gmail.com>
2024-03-28 01:34:54 -0300

    feat: Add Field model and FieldType enum

--- app/src/main/java/dev/vinicius/busycardapp/domain/model/card/Field.kt
+++ app/src/main/java/dev/vinicius/busycardapp/domain/model/card/Field.kt
@@ -2,33 +2,33 @@ package dev.vinicius.busycardapp.domain.model.card
 
 sealed class Field(
     open var name: String = "",
-    open var offsetX: Int = 0,
-    open var offsetY: Int = 0,
-    open var size: Int = 0,
+    open var offsetX: Float = 0f,
+    open var offsetY: Float = 0f,
+    open var size: Float = 0f,
 ) {
     data class AddressField(
         override var name: String = "",
-        override var offsetX: Int = 0,
-        override var offsetY: Int = 0,
-        override var size: Int = 0,
+        override var offsetX: Float = 0f,
+        override var offsetY: Float = 0f,
+        override var size: Float = 0f,
         var localization: Pair<Long, Long>,
         var textLocalization: String = "",
     ): Field(name, offsetX, offsetY, size)
 
     data class ImageField(
         override var name: String = "",
-        override var offsetX: Int = 0,
-        override var offsetY: Int = 0,
-        override var size: Int = 0,
+        override var offsetX: Float = 0f,
+        override var offsetY: Float = 0f,
+        override var size: Float = 0f,
         var imageUrl: String = "",
     ): Field(name, offsetX, offsetY, size)
 
     data class TextField(
         override var name: String = "",
-        override var offsetX: Int = 0,
-        override var offsetY: Int = 0,
-        override var size: Int = 0,
-        var textType: TextType,
+        override var offsetX: Float = 0f,
+        override var offsetY: Float = 0f,
+        override var size: Float = 0f,
+        var textType: TextType = TextType.TEXT,
         var value: String = "",
     ): Field(name, offsetX, offsetY, size)
 }
---
+++ app/src/main/java/dev/vinicius/busycardapp/domain/model/card/FieldType.kt
@@ -0,0 +1,9 @@
+package dev.vinicius.busycardapp.domain.model.card
+
+enum class FieldType {
+    TEXT,
+    PHONE,
+    EMAIL,
+    ADDRESS,
+    IMAGE,
+}
