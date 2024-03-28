a4f107a3d8ae28425a3e6780cfd20a2ee0f9204e
ViniciusJMR <viniciusdejesus605@gmail.com>
2024-03-28 01:40:17 -0300

    feat: Add GetCardById and SaveCard UseCase

---
+++ app/src/main/java/dev/vinicius/busycardapp/domain/model/usecase/card/GetCardById.kt
@@ -0,0 +1,16 @@
+package dev.vinicius.busycardapp.domain.model.usecase.card
+
+import dev.vinicius.busycardapp.core.UseCase
+import dev.vinicius.busycardapp.data.repository.Repository
+import dev.vinicius.busycardapp.domain.model.card.Card
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetCardById @Inject constructor(
+    private val repository: Repository<Long, Card>
+): UseCase<Long, Card>() {
+    override suspend fun execute(param: Long) = flow {
+        emit(repository.getById(param))
+    }
+}
---
+++ app/src/main/java/dev/vinicius/busycardapp/domain/model/usecase/card/SaveCard.kt
@@ -0,0 +1,16 @@
+package dev.vinicius.busycardapp.domain.model.usecase.card
+
+import dev.vinicius.busycardapp.core.UseCase
+import dev.vinicius.busycardapp.data.repository.Repository
+import dev.vinicius.busycardapp.domain.model.card.Card
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class SaveCard @Inject constructor(
+    private val repository: Repository<Long, Card>
+): UseCase.NoSource<Card>() {
+    override suspend fun execute(param: Card) = flow {
+        emit(repository.save(param))
+    }
+}
