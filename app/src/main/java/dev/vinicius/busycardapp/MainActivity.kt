0e5a4d64d1fb8b5ef8fa30b4ead450665380fb71
ViniciusJMR <viniciusdejesus605@gmail.com>
2024-03-29 21:57:51 -0300

    WIP: CardCreationScreen - Using DraggableComponent; Add support to changes on CardCreationEvent

--- app/src/main/java/dev/vinicius/busycardapp/MainActivity.kt
+++ app/src/main/java/dev/vinicius/busycardapp/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,16 +11,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -33,14 +33,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -49,19 +46,13 @@ import dev.vinicius.busycardapp.domain.model.card.FieldType
 import dev.vinicius.busycardapp.presentation.card_creation.CardCreationEvent
 import dev.vinicius.busycardapp.presentation.card_creation.CardCreationState
 import dev.vinicius.busycardapp.presentation.card_creation.CardCreationViewModel
+import dev.vinicius.busycardapp.presentation.card_creation.component.DraggableFieldComponent
 import dev.vinicius.busycardapp.ui.theme.BusyCardAppTheme
-import kotlin.math.roundToInt
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val _list = listOf(
-            Field.TextField(offsetX = 14f, offsetY = 200f, value = "Pinto pinto"),
-            Field.TextField(offsetX = 0f, offsetY = 0f, value = "Pinto pinto"),
-        ).toMutableStateList()
-        val list: List<Field> = _list
-
         setContent {
             BusyCardAppTheme {
                 val viewModel = hiltViewModel<CardCreationViewModel>()
@@ -79,14 +70,13 @@ class MainActivity : ComponentActivity() {
 }
 
 
-
 @Composable
 fun SelectableField(
     modifier: Modifier = Modifier,
     text: String,
     onClick: (FieldType) -> Unit,
     fieldType: FieldType,
-){
+) {
 
     TextButton(
         modifier = modifier,
@@ -104,58 +94,58 @@ fun SelectFieldMenu(
     modifier: Modifier = Modifier,
     onClickToAdd: (FieldType) -> Unit,
 ) {
-        Column(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(4.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        )
-         {
-            Row(
-                modifier = Modifier.padding(top = 6.dp),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                SelectableField(
-                    modifier = Modifier.weight(1f),
-                    text = "Texto",
-                    onClick = onClickToAdd,
-                    fieldType = FieldType.TEXT,
-                )
-                SelectableField(
-                    modifier = Modifier.weight(1f),
-                    text = "Telefone",
-                    onClick = onClickToAdd,
-                    fieldType = FieldType.PHONE,
-                )
-            }
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(4.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    )
+    {
+        Row(
+            modifier = Modifier.padding(top = 6.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            SelectableField(
+                modifier = Modifier.weight(1f),
+                text = "Texto",
+                onClick = onClickToAdd,
+                fieldType = FieldType.TEXT,
+            )
+            SelectableField(
+                modifier = Modifier.weight(1f),
+                text = "Telefone",
+                onClick = onClickToAdd,
+                fieldType = FieldType.PHONE,
+            )
+        }
 
-            Row (
-                modifier = Modifier.padding(top = 6.dp),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ){
-                SelectableField(
-                    modifier = Modifier.weight(1f),
-                    text = "Email",
-                    onClick = onClickToAdd,
-                    fieldType = FieldType.EMAIL,
-                )
-                SelectableField(
-                    modifier = Modifier.weight(1f),
-                    text = "Endereço",
-                    onClick = onClickToAdd,
-                    fieldType = FieldType.ADDRESS,
-                )
-            }
+        Row(
+            modifier = Modifier.padding(top = 6.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            SelectableField(
+                modifier = Modifier.weight(1f),
+                text = "Email",
+                onClick = onClickToAdd,
+                fieldType = FieldType.EMAIL,
+            )
+            SelectableField(
+                modifier = Modifier.weight(1f),
+                text = "Endereço",
+                onClick = onClickToAdd,
+                fieldType = FieldType.ADDRESS,
+            )
+        }
 
-             Row(
-                 modifier = Modifier.padding(top = 6.dp),
-                 horizontalArrangement = Arrangement.SpaceEvenly
-             ){
-                 SelectableField(
-                     modifier = Modifier.weight(1f),
-                     text = "Imagem", onClick = onClickToAdd, fieldType = FieldType.IMAGE
-                 )
-             }
+        Row(
+            modifier = Modifier.padding(top = 6.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            SelectableField(
+                modifier = Modifier.weight(1f),
+                text = "Imagem", onClick = onClickToAdd, fieldType = FieldType.IMAGE
+            )
+        }
     }
 }
 
@@ -165,17 +155,17 @@ fun CardSurface(
     fields: List<Field>,
     onDragField: (Field) -> Unit,
 ) {
-    Surface (
+    Surface(
         modifier
             .fillMaxWidth()
             .padding(start = 4.dp, end = 4.dp)
             .background(color = Color.LightGray),
         shape = RoundedCornerShape(5.dp)
     ) {
-        Box (
+        Box(
             modifier = Modifier.height(200.dp)
-        ){
-            fields.forEach {field ->
+        ) {
+            fields.forEach { field ->
                 HandleFields(
                     field = field,
                     onDragField = onDragField
@@ -191,34 +181,13 @@ fun HandleFields(
     field: Field,
     onDragField: (Field) -> Unit,
 ) {
-    var offsetX by remember { mutableStateOf(field.offsetX) }
-    var offsetY by remember { mutableStateOf(field.offsetX) }
-    Box (
-        modifier = Modifier
-            .offset {
-                IntOffset(
-                    offsetX.roundToInt(),
-                    offsetY.roundToInt()
-                )
-            }
-            .pointerInput(Unit) {
-                detectDragGestures { change, dragAmount ->
-                    change.consume()
-                    offsetX += dragAmount.x
-                    offsetY += dragAmount.y
-                    field.offsetX = offsetX
-                    field.offsetY = offsetY
-                    onDragField(field)
-                }
-            },
-    ){
-        when(field) {
+    DraggableFieldComponent(field = field, onDragField = onDragField) {
+        when (field) {
             is Field.AddressField -> TODO()
             is Field.ImageField -> TODO()
-            is Field.TextField -> { TextField(field = field) }
+            is Field.TextField -> TextField(field = field)
         }
     }
-
 }
 
 @Composable
@@ -238,14 +207,53 @@ fun ShowCardSection(
     event: (CardCreationEvent) -> Unit,
     state: CardCreationState,
 ) {
-    
+
     CardSurface(
         fields = state.cardFields,
         onDragField = {
-            event(CardCreationEvent.OnSelectField(it))
+            event(CardCreationEvent.CardEvent.OnSelectField(it))
         }
     )
-    
+
+}
+
+
+@Composable
+fun TextFieldMenu(
+    modifier: Modifier = Modifier,
+    onChangeText: (CardCreationEvent.FieldEvent) -> Unit,
+    field: Field.TextField,
+) {
+    var fieldValue by remember { mutableStateOf(field.value) }
+    Column {
+        OutlinedTextField(
+            value = fieldValue,
+            onValueChange = {
+                fieldValue = it
+                field.value = it
+            }
+        )
+        Button(
+            onClick = {
+                onChangeText(CardCreationEvent.FieldEvent.OnTextFieldChange(field))
+            }
+        ) {
+            Text("Confirm")
+        }
+    }
+}
+
+@Composable
+fun FieldInfoMenu(
+    modifier: Modifier = Modifier,
+    field: Field,
+    onChangeField: (CardCreationEvent.FieldEvent) -> Unit
+) {
+    when(field){
+        is Field.AddressField -> TODO()
+        is Field.ImageField -> TODO()
+        is Field.TextField -> TextFieldMenu(onChangeText = onChangeField , field = field)
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -253,18 +261,22 @@ fun ShowCardSection(
 fun OptionsSection(
     modifier: Modifier = Modifier,
     onAddField: (FieldType) -> Unit,
+    onChangeField: (CardCreationEvent.FieldEvent) -> Unit,
     onDismissModalSheet: () -> Unit,
     currentlySelectedField: Field?,
     showBottomSheet: Boolean,
 ) {
     val sheetState = rememberModalBottomSheetState()
-    if (showBottomSheet){
+    if (showBottomSheet) {
         ModalBottomSheet(
             onDismissRequest = onDismissModalSheet,
             sheetState = sheetState
-            ) {
-            if (currentlySelectedField != null){
-                Text("Tela de opções do menu")
+        ) {
+            if (currentlySelectedField != null) {
+                FieldInfoMenu(
+                    field = currentlySelectedField,
+                    onChangeField = onChangeField
+                )
             } else {
                 SelectFieldMenu(
                     modifier = Modifier.padding(bottom = 8.dp),
@@ -273,7 +285,22 @@ fun OptionsSection(
             }
         }
     }
+}
 
+@Preview(showBackground = true)
+@Composable
+private fun OptionsSectionPreview() {
+    BusyCardAppTheme {
+        var currentlySelectedField by remember { mutableStateOf<Field?>(null) }
+        if (currentlySelectedField != null) {
+            FieldInfoMenu(field = currentlySelectedField!!, onChangeField = {})
+        } else {
+            SelectFieldMenu(
+                modifier = Modifier.padding(bottom = 8.dp),
+                onClickToAdd = {currentlySelectedField = Field.TextField()}
+            )
+        }
+    }
 }
 
 
@@ -284,23 +311,24 @@ fun Screen(
     event: (CardCreationEvent) -> Unit,
 ) {
     val TAG = "CardCreationScreen"
-    Scaffold (
+    Scaffold(
         floatingActionButton = {
             ExtendedFloatingActionButton(
                 text = { Text("Add Field") },
                 icon = { Icon(Icons.Filled.Add, contentDescription = "") },
-                onClick = { event(CardCreationEvent.OnShowBottomSheet()) }
+                onClick = { event(CardCreationEvent.ModalEvent.OnShowBottomSheet()) }
             )
         }
     ) {
-        Box (modifier.padding(it)){
+        Box(modifier.padding(it)) {
             ShowCardSection(
                 event = event,
                 state = state
             )
             OptionsSection(
-                onAddField = {fieldType -> event(CardCreationEvent.OnAddField(fieldType))},
-                onDismissModalSheet = { event(CardCreationEvent.OnDismissModalSheet) },
+                onAddField = { fieldType -> event(CardCreationEvent.CardEvent.OnAddField(fieldType)) },
+                onChangeField = {fieldEvent -> event(fieldEvent)},
+                onDismissModalSheet = { event(CardCreationEvent.ModalEvent.OnDismissModalSheet) },
                 currentlySelectedField = state.currentlySelectedField,
                 showBottomSheet = state.showBottomSheet
             )
