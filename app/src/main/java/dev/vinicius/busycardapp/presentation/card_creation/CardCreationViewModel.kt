0c54d678ea6f95be041c680a38f969d2ea95eb1e
ViniciusJMR <viniciusdejesus605@gmail.com>
2024-03-29 21:27:41 -0300

    edit(CardCreationViewModel): Support for new Events categories

--- app/src/main/java/dev/vinicius/busycardapp/presentation/card_creation/CardCreationViewModel.kt
+++ app/src/main/java/dev/vinicius/busycardapp/presentation/card_creation/CardCreationViewModel.kt
@@ -1,15 +1,11 @@
 package dev.vinicius.busycardapp.presentation.card_creation
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.vinicius.busycardapp.domain.model.card.Field
 import dev.vinicius.busycardapp.domain.model.card.FieldType
 import dev.vinicius.busycardapp.domain.model.card.TextType
 import dev.vinicius.busycardapp.domain.model.usecase.card.SaveCard
-import dev.vinicius.busycardapp.presentation.card_creation.CardCreationEvent.OnAddField
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -25,84 +21,104 @@ class CardCreationViewModel @Inject constructor(
 
     fun onEvent(event: CardCreationEvent) {
         when (event) {
-            is OnAddField -> handleOnAddField(event.fieldType)
-            is CardCreationEvent.OnDeleteField -> handleOnDelete(event.field)
-            is CardCreationEvent.OnSelectField -> handleOnSelectField(event.field)
-            CardCreationEvent.OnDismissModalSheet -> handleOnDismissModalSheet()
-            is CardCreationEvent.OnShowBottomSheet -> handleOnShowBottomSheet(event.field)
+            is CardCreationEvent.CardEvent -> handleCardEvent(event)
+            is CardCreationEvent.FieldEvent -> handleOnChangeFieldValue(event)
+            is CardCreationEvent.ModalEvent -> handleOnModalEvent(event)
         }
     }
 
-    private fun handleOnShowBottomSheet(field: Field?) {
-        _state.update {
-            it.copy(
-                currentlySelectedField = field,
-                showBottomSheet = true,
-            )
-        }
-    }
-
-    private fun handleOnDismissModalSheet() {
-        _state.update {
-            it.copy(
-                currentlySelectedField = null,
-                showBottomSheet = false,
-            )
-        }
-
-    }
-
-    private fun handleOnAddField(fieldType: FieldType) {
-        val field: Field = when(fieldType) {
-            FieldType.TEXT -> {
-                Field.TextField(
-                    name = "text field 1",
-                    value = "Pintao",
-                    offsetX = 200f,
-                    offsetY = 200f,
-                    textType = TextType.TEXT
-                )
+    private fun handleCardEvent(event: CardCreationEvent.CardEvent) {
+        when (event) {
+            is CardCreationEvent.CardEvent.OnAddField -> {
+                val field: Field = when(event.fieldType) {
+                    FieldType.TEXT -> {
+                        Field.TextField(
+                            name = "text field 1",
+                            value = "Pintao",
+                            offsetX = 200f,
+                            offsetY = 200f,
+                            textType = TextType.TEXT
+                        )
+                    }
+                    FieldType.PHONE -> {
+                        Field.TextField(
+                            name = "Phone field 1",
+                            value = "(99) 99999-9999",
+                            offsetX = 200f,
+                            offsetY = 200f,
+                            textType = TextType.PHONE
+                        )
+                    }
+                    FieldType.EMAIL -> {
+                        Field.TextField(
+                            name = "Phone field 1",
+                            value = "(99) 99999-9999",
+                            offsetX = 200f,
+                            offsetY = 200f,
+                            textType = TextType.EMAIL
+                        )
+                    }
+                    FieldType.ADDRESS -> TODO()
+                    FieldType.IMAGE -> TODO()
+                }
+                // Check if this doesn't make the entire CardSurface component to recompose
+                val list = _state.value.cardFields.toMutableList()
+                list.add(field)
+                _state.update {
+                    it.copy(
+                        cardFields = list,
+                        currentlySelectedField = field
+                    )
+                }
             }
-            FieldType.PHONE -> {
-                Field.TextField(
-                    name = "Phone field 1",
-                    value = "(99) 99999-9999",
-                    offsetX = 200f,
-                    offsetY = 200f,
-                    textType = TextType.PHONE
-                )
+            is CardCreationEvent.CardEvent.OnDeleteField -> {}
+            is CardCreationEvent.CardEvent.OnSelectField -> {
+                _state.update {
+                    it.copy(
+                        currentlySelectedField = event.field
+                    )
+                }
             }
-            FieldType.EMAIL -> {
-                Field.TextField(
-                    name = "Phone field 1",
-                    value = "(99) 99999-9999",
-                    offsetX = 200f,
-                    offsetY = 200f,
-                    textType = TextType.EMAIL
-                )
+        }
+
+    }
+
+    private fun handleOnChangeFieldValue(event: CardCreationEvent.FieldEvent) {
+        when(event) {
+            is CardCreationEvent.FieldEvent.OnTextFieldChange -> {
+                _state.update {
+                    it.copy(
+                        currentlySelectedField =
+                        ( it.currentlySelectedField as Field.TextField )
+                            .apply {
+                                value = event.field.value
+                            },
+                        showBottomSheet = false
+                    )
+                }
             }
-            FieldType.ADDRESS -> TODO()
-            FieldType.IMAGE -> TODO()
-        }
-        val list = _state.value.cardFields.toMutableList()
-        list.add(field)
-        _state.update {
-            it.copy(
-                cardFields = list
-            )
         }
     }
 
-    private fun handleOnSelectField(field: Field?) {
-        _state.update {
-            it.copy(
-                currentlySelectedField = field
-            )
+    // Analisar eficacia de utilizar classe Effect para assumir eventos na tela
+    private fun handleOnModalEvent(event: CardCreationEvent.ModalEvent) {
+        when(event) {
+            CardCreationEvent.ModalEvent.OnDismissModalSheet -> {
+                _state.update {
+                    it.copy(
+                        currentlySelectedField = null,
+                        showBottomSheet = false,
+                    )
+                }
+            }
+            is CardCreationEvent.ModalEvent.OnShowBottomSheet -> {
+                _state.update {
+                    it.copy(
+                        currentlySelectedField = event.field,
+                        showBottomSheet = true,
+                    )
+                }
+            }
         }
     }
-
-    private fun handleOnDelete(field: Field) {
-
-    }
-
 }
