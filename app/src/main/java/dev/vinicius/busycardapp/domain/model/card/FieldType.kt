7e682c467b262845db6d33a8641e714de91704e7
ViniciusJMR <viniciusdejesus605@gmail.com>
2024-04-02 20:17:05 -0300

    edit: remove constructor param of TextType enum

--- app/src/main/java/dev/vinicius/busycardapp/domain/model/card/FieldType.kt
+++ app/src/main/java/dev/vinicius/busycardapp/domain/model/card/FieldType.kt
@@ -1,5 +1,6 @@
 package dev.vinicius.busycardapp.domain.model.card
 
+//Retirar classe verificar mudanças necessárias na Criação de cards
 enum class FieldType {
     TEXT,
     PHONE,
--- app/src/main/java/dev/vinicius/busycardapp/domain/model/card/TextType.kt
+++ app/src/main/java/dev/vinicius/busycardapp/domain/model/card/TextType.kt
@@ -1,7 +1,7 @@
 package dev.vinicius.busycardapp.domain.model.card
 
-enum class TextType(val type: String) {
-    TEXT("text"),
-    PHONE("phone"),
-    EMAIL("email")
+enum class TextType {
+    TEXT,
+    PHONE,
+    EMAIL,
 }
