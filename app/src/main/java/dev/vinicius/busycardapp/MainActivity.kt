1140eeb50dbad3b0dd6f0cf59697e7bf64c4f83d
ViniciusJMR <viniciusdejesus605@gmail.com>
2024-04-09 01:31:47 -0300

    edit: Change MainActivity shown screen

--- app/src/main/java/dev/vinicius/busycardapp/MainActivity.kt
+++ app/src/main/java/dev/vinicius/busycardapp/MainActivity.kt
@@ -3,59 +3,17 @@ package dev.vinicius.busycardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.rememberModalBottomSheetState
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import dev.vinicius.busycardapp.domain.model.card.Field
-import dev.vinicius.busycardapp.domain.model.card.FieldType
-import dev.vinicius.busycardapp.domain.model.card.TextType
-import dev.vinicius.busycardapp.presentation.card_creation.CardCreationEvent
-import dev.vinicius.busycardapp.presentation.card_creation.CardCreationState
 import dev.vinicius.busycardapp.presentation.card_creation.CardCreationViewModel
-import dev.vinicius.busycardapp.presentation.card_creation.Screen
-import dev.vinicius.busycardapp.presentation.card_creation.component.DefaultDialog
-import dev.vinicius.busycardapp.presentation.card_creation.component.DraggableFieldComponent
-import dev.vinicius.busycardapp.presentation.card_creation.component.RadioOptions
-import dev.vinicius.busycardapp.presentation.card_creation.component.SelectableOption
-import dev.vinicius.busycardapp.presentation.card_creation.section.OptionsSection
-import dev.vinicius.busycardapp.presentation.card_creation.section.ShowCardSection
+import dev.vinicius.busycardapp.presentation.card_creation.CardCreationScreen
+import dev.vinicius.busycardapp.presentation.card_info.CardInfoScreen
 import dev.vinicius.busycardapp.ui.theme.BusyCardAppTheme
 
 @AndroidEntryPoint
@@ -71,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Screen(state = state, event = viewModel::onEvent)
+                    CardInfoScreen()
                 }
             }
         }
