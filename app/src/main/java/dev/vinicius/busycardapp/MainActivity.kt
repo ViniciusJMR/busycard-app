5b15fb8fc51158a52dd2bb7a1497e0c18a77f0c0
ViniciusJMR <viniciusdejesus605@gmail.com>
2024-04-12 22:31:21 -0300

    feat: Add Simple BottomNavBar for Home Screen

--- app/src/main/java/dev/vinicius/busycardapp/MainActivity.kt
+++ app/src/main/java/dev/vinicius/busycardapp/MainActivity.kt
@@ -3,17 +3,35 @@ package dev.vinicius.busycardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CardMembership
+import androidx.compose.material.icons.outlined.CreditCard
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.navigation.navigate
+import com.ramcosta.composedestinations.navigation.popUpTo
+import com.ramcosta.composedestinations.spec.DirectionDestinationSpec
 import dagger.hilt.android.AndroidEntryPoint
-import dev.vinicius.busycardapp.presentation.card_creation.CardCreationViewModel
-import dev.vinicius.busycardapp.presentation.card_creation.CardCreationScreen
-import dev.vinicius.busycardapp.presentation.card_info.CardInfoScreen
+import dev.vinicius.busycardapp.presentation.NavGraphs
+import dev.vinicius.busycardapp.presentation.appCurrentDestinationAsState
+import dev.vinicius.busycardapp.presentation.destinations.MyCardsScreenDestination
+import dev.vinicius.busycardapp.presentation.destinations.SharedCardsScreenDestination
+import dev.vinicius.busycardapp.presentation.startAppDestination
 import dev.vinicius.busycardapp.ui.theme.BusyCardAppTheme
 
 @AndroidEntryPoint
@@ -22,15 +40,60 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BusyCardAppTheme {
-                val viewModel = hiltViewModel<CardCreationViewModel>()
-                val state by viewModel.state.collectAsState()
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                val navController = rememberNavController()
+                Scaffold(
+                    bottomBar = {
+                        HomeBottomNavBar(navController = navController)
+                    }
                 ) {
-                    CardInfoScreen()
+                    Surface(
+                        modifier = Modifier.fillMaxSize().padding(it),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        DestinationsNavHost(
+                            navGraph = NavGraphs.root,
+                            navController = navController
+                        )
+                    }
                 }
+            }
+        }
+    }
+
+    private enum class BottomBarDestination(
+        val direction: DirectionDestinationSpec,
+        val icon: ImageVector,
+        @StringRes val label: Int
+    ) {
+        SharedCards(SharedCardsScreenDestination, Icons.Filled.CardMembership, R.string.label_bottomnav_shared_screen),
+        MyCards(MyCardsScreenDestination, Icons.Outlined.CreditCard, R.string.label_bottomnav_mycards_screen)
+    }
+
+    @Composable
+    private fun HomeBottomNavBar(
+        navController: NavController
+    ) {
+        val currentDestination = navController.appCurrentDestinationAsState().value
+            ?: NavGraphs.root.startAppDestination
+        NavigationBar {
+            BottomBarDestination.entries.forEach { destination ->
+                NavigationBarItem(
+                    selected = currentDestination == destination.direction,
+                    onClick = {
+                        navController.navigate(destination.direction) {
+                            popUpTo(NavGraphs.root) {
+                                saveState = true
+                            }
+
+                            launchSingleTop = true
+
+                            restoreState = true
+                        }
+                    },
+                    icon = { Icon(destination.icon, contentDescription = stringResource(destination.label)) },
+                    label = { Text(stringResource(destination.label)) }
+                )
             }
         }
     }
