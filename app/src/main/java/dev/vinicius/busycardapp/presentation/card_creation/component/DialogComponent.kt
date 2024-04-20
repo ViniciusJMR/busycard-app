341f2b5b08322f04d3e57346d7729c2ddbad0709
ViniciusJMR <viniciusdejesus605@gmail.com>
2024-04-20 02:37:37 -0300

    feat: Add Card Image selection on CardCreationScreen to be saved

--- app/src/main/java/dev/vinicius/busycardapp/presentation/card_creation/component/DialogComponent.kt
+++ app/src/main/java/dev/vinicius/busycardapp/presentation/card_creation/component/DialogComponent.kt
@@ -1,16 +1,31 @@
 package dev.vinicius.busycardapp.presentation.card_creation.component
 
 import android.app.Dialog
+import android.net.Uri
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Image
+import androidx.compose.material.icons.outlined.PhotoCamera
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -21,16 +36,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import coil.compose.rememberAsyncImagePainter
 import dev.vinicius.busycardapp.R
 import dev.vinicius.busycardapp.domain.model.card.TextType
+import dev.vinicius.busycardapp.ui.theme.BusyCardAppTheme
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -88,19 +110,78 @@ fun FullScreenDialog(
 @Composable
 fun CardInfoDialog(
     modifier: Modifier = Modifier,
-    onConfirmation: (String) -> Unit,
+    onConfirmation: (String, Uri?) -> Unit,
     onDismiss: () -> Unit,
     cardName: String,
-
+    cardImageUri: Uri?,
 ) {
     var newCardName by remember { mutableStateOf(cardName) }
+    var newImageUri by remember { mutableStateOf(cardImageUri) }
     FullScreenDialog(onDismissRequest = onDismiss) {
         Column {
+            CardImage(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                onImageChange = {
+                    newImageUri = it
+                },
+                newImageUri,
+            )
             OutlinedTextField(value = newCardName, onValueChange = { newCardName = it })
+            Spacer(modifier = Modifier.padding(8.dp))
 
-            Button(onClick = { onConfirmation(newCardName) }) {
+
+            Button(
+                onClick = {
+                    onConfirmation(newCardName, newImageUri)
+                }
+            ) {
                 Text(stringResource(R.string.txt_label_confirm))
             }
         }
     }
+}
+
+@Composable
+fun CardImage(
+    modifier: Modifier = Modifier,
+    onImageChange: (Uri?) -> Unit,
+    cardImageUri: Uri?
+) {
+    val TAG = "CardImage"
+    val imageUri = rememberSaveable { mutableStateOf(cardImageUri?.toString() ?: "") }
+    val painter = rememberAsyncImagePainter(
+        imageUri.value.ifEmpty { R.drawable.outline_image_24 }
+    )
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) {uri: Uri? ->
+        uri?.let {
+            imageUri.value = it.toString()
+            onImageChange(it)
+        }
+    }
+    Column (modifier = modifier) {
+        Image(
+            painter = painter,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .clip(CircleShape) // TODO: Change to param
+                .size(150.dp)
+        )
+        IconButton(onClick = { launcher.launch("image/*") }) {
+            Icon(
+                Icons.Outlined.PhotoCamera,
+                contentDescription = null
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CardInfoPreview() {
+   BusyCardAppTheme {
+//       CardImage(modifier = Modifier.fillMaxSize())
+   }
 }
