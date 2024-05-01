81e07fc454c876931778b11fa065f44dca3ba9b1
ViniciusJMR <viniciusdejesus605@gmail.com>
2024-05-01 20:07:54 -0300

    refactor: Change Firebase Realtime db to Firestore for CardRepository

--- app/src/main/java/dev/vinicius/busycardapp/data/repository/impl/CardRepository.kt
+++ app/src/main/java/dev/vinicius/busycardapp/data/repository/impl/CardRepository.kt
@@ -3,11 +3,13 @@ package dev.vinicius.busycardapp.data.repository.impl
 import android.util.Log
 import com.google.firebase.database.getValue
 import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dev.vinicius.busycardapp.data.remote.firebase.db.mapper.mapDomainFieldsToFirebaseModel
 import dev.vinicius.busycardapp.data.remote.firebase.db.mapper.mapToDomainModel
 import dev.vinicius.busycardapp.data.remote.firebase.db.mapper.mapToFirebaseModel
 import dev.vinicius.busycardapp.data.remote.firebase.db.model.FirebaseCardModel
+import dev.vinicius.busycardapp.data.remote.firebase.db.model.FirebaseFieldModel
 import dev.vinicius.busycardapp.domain.repository.IRepository
 import dev.vinicius.busycardapp.domain.model.card.Card
 import dev.vinicius.busycardapp.domain.model.card.Field
@@ -15,6 +17,7 @@ import dev.vinicius.busycardapp.domain.repository.Bucket
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.tasks.await
+import java.util.UUID
 import javax.inject.Inject
 
 class CardRepository @Inject constructor(
@@ -22,22 +25,21 @@ class CardRepository @Inject constructor(
 ): IRepository<String, Card> {
 
     // TODO: move it to interface
-    private val database = Firebase.database.reference
+    private val database = Firebase.firestore
 
     companion object {
         val TAG = "CardRepository"
     }
 
     override suspend fun getAll(): Flow<List<Card>> = flow {
-        val dataSnapshot = database.child("cards").get()
+        val dataSnapshot = database.collection("cards").get()
         dataSnapshot.await()
 
         when {
             dataSnapshot.isSuccessful -> {
-                val firebaseCardsMap = dataSnapshot.result.getValue<HashMap<String, FirebaseCardModel>>()
-                val firebaseCards = firebaseCardsMap?.entries?.map { it.value }
+                val firebaseCardsMap = dataSnapshot.result.toObjects(FirebaseCardModel::class.java)
                 // TODO: Create new Card model for Card Meta data
-                val cards = firebaseCards?.map { it.mapToDomainModel(emptyList()) } ?: emptyList()
+                val cards = firebaseCardsMap.map { it.mapToDomainModel(emptyList()) }
                 emit(cards)
             }
             dataSnapshot.isCanceled -> {
@@ -47,17 +49,16 @@ class CardRepository @Inject constructor(
     }
 
     override suspend fun getById(id: String): Flow<Card> = flow {
-        val cardsSnapshot = database.child("cards").child(id).get()
-        val fieldsSnapshot = database.child("fields").child(id).get()
+        val cardsSnapshot = database.collection("cards").document(id).get()
+        val fieldsSnapshot = database.collection("fields").document(id).get()
         cardsSnapshot.await()
         fieldsSnapshot.await()
 
         when {
             cardsSnapshot.isSuccessful -> {
-                val firebaseCard = cardsSnapshot.result.getValue<FirebaseCardModel>()
-                val firebaseFields = fieldsSnapshot.result
-                    .getValue<List<Map<String, Any>>>()
-                    ?: emptyList()
+                val firebaseCard = cardsSnapshot.result.toObject(FirebaseCardModel::class.java)
+                Log.d(TAG, "getById: fields: ${fieldsSnapshot.result.data?.get("list")}")
+                val firebaseFields: List<Map<String, Any>> = fieldsSnapshot.result.data?.get("list") as List<Map<String, Any>>
                 val card = firebaseCard!!.mapToDomainModel(firebaseFields)
                 emit(card)
             }
@@ -68,7 +69,8 @@ class CardRepository @Inject constructor(
     }
 
     override suspend fun save(item: Card) {
-        val key = database.child("cards").push().key
+        database.collection("cards")
+        val key = UUID.randomUUID().toString()
         item.id = key
 
         item.image.uri = item.image.uri?.let {
@@ -91,14 +93,15 @@ class CardRepository @Inject constructor(
 
 
         database
-            .child("cards")
-            .child(item.id.toString())
-            .setValue(item.mapToFirebaseModel())
+            .collection("cards")
+            .document(item.id.toString())
+            .set(item.mapToFirebaseModel())
+
 
         database
-            .child("fields")
-            .child(item.id.toString())
-            .setValue(mapDomainFieldsToFirebaseModel(item.fields))
+            .collection("fields")
+            .document(item.id.toString())
+            .set(mapOf("list" to mapDomainFieldsToFirebaseModel(item.fields)))
     }
 
 }
