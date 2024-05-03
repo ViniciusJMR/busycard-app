9744aeaa132b39e05af53c39b358a3ae16cfe632
ViniciusJMR <viniciusdejesus605@gmail.com>
2024-05-03 18:52:07 -0300

    feat: Change SaveCard usecase to save cardId on User myCards field

--- app/src/main/java/dev/vinicius/busycardapp/domain/usecase/card/SaveCard.kt
+++ app/src/main/java/dev/vinicius/busycardapp/domain/usecase/card/SaveCard.kt
@@ -1,21 +1,28 @@
 package dev.vinicius.busycardapp.domain.usecase.card
 
 import dev.vinicius.busycardapp.core.UseCase
-import dev.vinicius.busycardapp.domain.repository.IRepository
 import dev.vinicius.busycardapp.domain.model.card.Card
+import dev.vinicius.busycardapp.domain.model.user.User
+import dev.vinicius.busycardapp.domain.repository.Auth
+import dev.vinicius.busycardapp.domain.repository.IRepository
+import dev.vinicius.busycardapp.domain.repository.IUserRepository
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class SaveCard @Inject constructor(
-    private val repository: IRepository<String, Card>,
+    private val auth: Auth,
+    private val cardRepository: IRepository<String, Card>,
+    private val userRepository: IUserRepository<String, User>,
 ): UseCase.NoSource<Card>() {
     override suspend fun execute(param: Card) = flow {
         // Validation
 
         // Change owner to logged user
+        param.owner = auth.getCurrentUserId()
 
         // Save
-        param.owner = "Vinicius"
-        emit(repository.save(param))
+        val key = cardRepository.save(param)
+        userRepository.saveMyCardId(auth.getCurrentUserId(), key)
+        emit(Unit)
     }
 }
