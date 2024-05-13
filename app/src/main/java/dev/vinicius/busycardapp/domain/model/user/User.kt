e7c1ac0f20cd8b8dbe125b862692e4f7d2e6ab7f
ViniciusJMR <viniciusdejesus605@gmail.com>
2024-05-13 15:10:09 -0300

    feat: Add SharedCard Save and Delete on UserRepository

--- app/src/main/java/dev/vinicius/busycardapp/domain/model/user/User.kt
+++ app/src/main/java/dev/vinicius/busycardapp/domain/model/user/User.kt
@@ -6,5 +6,6 @@ data class User (
     var name: String = "",
     var surname: String = "",
     var email: String = "",
-    val myCards: List<String> = emptyList()
+    val myCards: List<String> = emptyList(),
+    val sharedCards: List<String> = emptyList(),
 )
--- app/src/main/java/dev/vinicius/busycardapp/domain/repository/IUserRepository.kt
+++ app/src/main/java/dev/vinicius/busycardapp/domain/repository/IUserRepository.kt
@@ -4,4 +4,12 @@ interface IUserRepository<K, T>: IRepository<K, T> {
     suspend fun saveMyCardId(userId: K, cardId: K)
 
     suspend fun getMyCardsId(userId: K): List<K>
+
+    suspend fun saveSharedCardId(userId: K, cardId: K)
+
+    suspend fun removeSharedCardId(userId: K, cardId: K)
+
+    suspend fun getSharedCardsId(userId: K): List<K>
+
+    suspend fun getUser(userId: K): T
 }
