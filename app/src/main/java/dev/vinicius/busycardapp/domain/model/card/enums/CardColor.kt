282fae4482a4af4972698df54ba4b9d271a1049e
ViniciusJMR <viniciusdejesus605@gmail.com>
2024-05-24 00:12:57 -0300

    edit: change CardColor values

--- app/src/main/java/dev/vinicius/busycardapp/domain/model/card/enums/CardColor.kt
+++ app/src/main/java/dev/vinicius/busycardapp/domain/model/card/enums/CardColor.kt
@@ -8,9 +8,9 @@ enum class CardColor (val color: Long) {
     DarkGray(0XFF444444),
     Gray    (0XFF888888),
     LightGray(0XFFCCCCCC),
-    Red     (0XFFFF0000),
-    Green   (0XFF04D404),
+    Red     (0XFFBF190D),
+    Green   (0XFF0BB30B),
     Blue    (0XFF0909E0),
-    Yellow  (0XFFE3E30B),
+    Yellow  (0XFFCFCF13),
     Cyan    (0XFF08C9C9),
 }
